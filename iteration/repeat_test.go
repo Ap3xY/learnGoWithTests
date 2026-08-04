8223647a79f62c20915f88bdf6e60145bd6fa6e9
Alex van den Berg <alexvdberg010109@gmail.com>
2026-08-04 13:04:21 +0200

    test(repeat): add example and benchmark test for repeat function

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,6 +1,9 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
 	repeated := Repeat("a")
@@ -9,4 +12,14 @@ func TestRepeat(t *testing.T) {
 	if repeated != expected {
 		t.Errorf("expected '%s' but got '%s'", expected, repeated)
 	}
+}
+func BenchmarkRepeat(b *testing.B) {
+	for b.Loop() {
+		Repeat("a")
+	}
+}
+
+func ExampleRepeat() {
+	fmt.Println(Repeat("a"))
+	// Output: aaaaa
 }
